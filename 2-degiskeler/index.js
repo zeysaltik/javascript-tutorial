a3bcd5ffe1d98f443b75e7bb4a26c8a36f0155a7
zeysaltik <zeysaltik200@gmail.com>
2024-07-25 17:36:17 +0300

    2-degiskenler

---
+++ 2-degiskeler/index.js
@@ -0,0 +1,16 @@
+let isim = "Zeynep Nur"
+let soyisim = "Saltık"
+
+console.log(isim + " " + soyisim)
+
+let mudurAdi = "Zeynep"
+let sinifBaskani = "Deniz"
+
+console.log("Müdür Adı:", mudurAdi)
+console.log("Sınıf Başkanı:", sinifBaskani)
+ 
+let maas = 7000
+let zam = 0.1
+let zamliMaas = maas + maas * zam
+
+console.log(zamliMaas)
