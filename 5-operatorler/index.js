01ce647529f25d153dc631e854011caf64acaa71
zeysaltik <zeysaltik200@gmail.com>
2024-07-25 17:37:12 +0300

    5-operatorler

---
+++ 5-operatorler/index.js
@@ -0,0 +1,21 @@
+let number1 = 10
+let number2 = 20
+let number3 = 30
+let result
+result = number1
+result += number3 //number1 + number3
+result -= number3 //number1 - number3
+result /= number3 //number1 / number3
+result *= number3 //number1 * number3
+result %= number3//number3 % number1
+// result = ++number1
+// reult = nuber1++
+
+console.log(result)
+
+result = number1 == number2 //false
+result = number1 != number2 //true
+result = 10 == "10"
+result = 10 === "10"
+
+
