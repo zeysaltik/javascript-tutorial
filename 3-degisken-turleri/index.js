711d6e4d5208c8119eda3c85de56a32874853228
zeysaltik <zeysaltik200@gmail.com>
2024-07-25 17:36:34 +0300

    3-degisken-turleri

---
+++ 3-degisken-turleri/index.js
@@ -0,0 +1,17 @@
+let ogrenciIsmi = "Zeynep"
+console.log(typeof ogrenciIsmi) //string
+
+let sayi1 = "10"
+let sayi2 = "20"
+let toplam = Number(sayi1) + Number(sayi2)
+console.log(toplam)
+
+console.log(typeof true) //boolean
+console.log(typeof false) //boolean
+
+let sayi10 = 10
+let sayi20 = 20
+console.log(sayi10 > sayi10) //false
+
+let ogrenciIsmi1
+console.log(typeof ogrenciIsmi1)
