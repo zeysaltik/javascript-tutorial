159861022134c47abbaa0dac9d089ab5d4ae65ff
zeysaltik <zeysaltik200@gmail.com>
2024-07-26 12:51:21 +0300

    8-strings

---
+++ 8-strings/index.js
@@ -0,0 +1,11 @@
+const ad = "Zeynep Nur"
+const soyAd = "Saltık"
+const yas = 16
+const sehir = "İzmir"
+const meslek = "Öğrenci"
+
+const bio = "Benim adım " + ad+ "." + " Soyadım " + soyAd + ". " + sehir +"'de yaşıyorum. " + yas + " yaşındayım, "+ meslek + "yim."
+const newBio = `Benim adım ${ad}. Soyadım ${soyAd}. ${sehir}'de yaşıyorum. ${yas} yaşındayım, ${meslek}yim`
+
+console.log(bio)
+console.log(newBio)
