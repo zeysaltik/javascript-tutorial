56693e2131b1f7c35e6fbed9ec2998be8a23d983
zeysaltik <zeysaltik200@gmail.com>
2024-07-26 16:35:40 +0300

    12-numbers

---
+++ 12-numbers/index.js
@@ -0,0 +1,28 @@
+const username = "Zeynepnursaltikk"
+const yas = 16
+
+let result
+
+result = 16 
+result = Number("16")
+result = parseInt("16.7")
+console.log(result)
+result = parseFloat("16.7")
+console.log(result)
+result = parseInt("a27")
+result = isNaN("a27")
+
+let myNumber = 10.123456
+result = myNumber.toPrecision(5)
+result = myNumber.toFixed(3)
+result = Math.round(1.4)
+result = Math.round(1,6)
+result = Math.ceil(1,2)
+result = Math.floor(1.7)
+result = Math.pow(7, 3)
+result = Math.sqrt(25)
+result = Math.abs(-7)
+result = Math.max(10, 6, 8, 2)
+result = Math.min(10, 6, 8, 2)
+
+console.log(result)
