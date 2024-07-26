e5d70d679c9e553c0493c1fdd5d3573c39000adf
zeysaltik <zeysaltik200@gmail.com>
2024-07-26 11:15:39 +0300

    6-if-else

---
+++ 6-if-else/index.js
@@ -0,0 +1,26 @@
+let isLoggedİn = true
+
+if (isLoggedİn == true) {
+    console.log("çalıştı")
+} else {
+    console.log("true değiş")
+}
+
+const userName = "zeynepnursaltikk"
+const password = "zeynepnursaltikk123"
+
+if (userName == "zeynepnursaltikk") {
+    if (password == "zeynepnursaltikk123") {
+        console.log("Giriş yapıldı.")
+    } else {
+        console.log("Şifre yanlış")
+    }
+} else {
+    console.log("Kullanıcı bulunamadı.")
+}
+
+
+
+
+
+
