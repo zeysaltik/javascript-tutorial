8a0644439726e86f2abdf4e990debeeeb2275d4d
zeysaltik <zeysaltik200@gmail.com>
2024-07-26 13:32:53 +0300

    9-string-metodlari

---
+++ 9-string-metodlari/index.js
@@ -0,0 +1,11 @@
+let instagram = "Zeynep nur saltikk"
+let result 
+result = instagram.toLowerCase()
+result = instagram.toUpperCase()
+result = instagram.length
+result = instagram.trim()
+result = instagram.slice(0, 6)
+result = instagram.split(" ")
+result = instagram[0]
+
+console.log(result)
