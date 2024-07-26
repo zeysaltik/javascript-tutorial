3ff33a186404316b28dae5d40881a3b0b45b595f
zeysaltik <zeysaltik200@gmail.com>
2024-07-26 15:48:31 +0300

    11-ternary-operator

---
+++ 11-ternary-operator/index.js
@@ -0,0 +1,11 @@
+const userName = "Zeynepnursaltikk"
+const password = "Zeynepnursaltikk123"
+
+password.length > 18 ? console.log("Çalıştı") : console.log("18'den büyük olmadığı için çaşılmadı")
+userName.length > 16 ? console.log("username doğru") : console.log("doğru değil")
+
+if (password.length > 20) {
+    console.log("Çalıştı")
+} else {
+    console.log("20'den büyük olmadığı için çalışmadı")
+}
