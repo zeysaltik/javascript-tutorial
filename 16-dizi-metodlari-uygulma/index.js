d153bc93b1cadf355f8c88d1e0c2e9b48b253355
zeysaltik <zeysaltik200@gmail.com>
2024-07-29 14:22:44 +0300

    16-dizi-metodlari-uygulama

---
+++ 16-dizi-metodlari-uygulma/index.js
@@ -0,0 +1,8 @@
+let customers = ["zeynep", "deniz", "leyla"]
+
+let customer1 = ["zeynep", ["asus", "monster", "del",[10, 20, 30]]]
+
+// console.log(`Ürün: ${customer1[1][0]} Fiyatı: ${customer1[1][3][0]}`)
+
+console.log(customers[0]) //ilk eleman
+console.log(customers[customers.length -1]) //son eleman
