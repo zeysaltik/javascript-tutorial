689f984bc22ff769af29eb846b2dda6c69340089
zeysaltik <zeysaltik200@gmail.com>
2024-07-29 11:52:28 +0300

    14-diziler

---
+++ 14-diziler/index.js
@@ -0,0 +1,13 @@
+const urun1 = "samsung"
+const urun2 = "apple"
+const urun3 = "huawei"
+
+const urunler = ["samsung", "apple", "huawei"]
+const fiyatlar = [10, 20, 30]
+const urunn = ["samsung", 10]
+
+const yeniUrun = ["samsung", 150, ["apple", 20]]
+
+console.log(yeniUrun[2][1])
+console.log(`Ürünümüz: ${urunler [2]} ${fiyatlar [2]}`)
+
