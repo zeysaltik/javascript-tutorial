c2e7e15a9f47b0dddaccee98e4bfdf3a313f4375
zeysaltik <zeysaltik200@gmail.com>
2024-07-31 10:21:44 +0300

    19-fonksiyonlar

--- 19-fonksiyonlar/index.js
+++ 19-fonksiyonlar/index.js
@@ -5,3 +5,18 @@ function sayilariTopla(sayi1, sayi2) {
 }
 sayilariTopla(3, 5)
 
+let toplam = 0
+function sayiToplami(...sayilar) {
+    for(let sayi of sayilar){
+        toplam += sayi
+    }
+}
+
+sayiToplami(3, 5, 7, 9, 11)
+console.log(toplam)
+
+function yas(dogumTarihi) {
+    return new Date().getFullYear() - dogumTarihi
+}
+const yasHesapla = yas(2008)
+console.log(yasHesapla)
