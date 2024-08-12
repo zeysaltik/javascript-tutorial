d503f386a5e934b07aa3632aadcd92f0974a5ac4
zeysaltik <zeysaltik200@gmail.com>
2024-08-12 12:47:58 +0300

    27-asenkron-ve-await-devami

--- 27-asenkron-ve-api/index.js
+++ 27-asenkron-ve-api/index.js
@@ -33,16 +33,45 @@
 // xhr.send()
 // console.log(myList);
 
-//! Promise
+// //! Promise
+// function getRandomNumber() {
+//     return new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//             const number = Math.floor(Math.random() * 100)
+//             if (number % 2 === 0) {
+//                 resolve(number)
+//             } else {
+//                 reject(`Hata: ${number} sayısı tek bir sayıdır.`)
+//             }
+//         }, 1000)
+//     })
+// }
 
-function getRandomNumber() {
-    new Promise((resolve, reject) => {
-        const number = Math.floor(Math.random() * 100)
-        if (number % 2 === 0) {
-            resolve()
-        } else {
-            console.log(number, "Tek sayı")
-        }
-    })
-}
-getRandomNumber().then((number) => console.log("Çözümlendi:" + number))
+// getRandomNumber()
+//     .then((number) => console.log("çözümlendi:" + number))
+//     .catch((error) => console.log(error))
+
+ //! Fetch
+// fetch("https://jsonplaceholder.typicode.com/users")
+//     .then(response => response.json())
+//     .then(data => console.log(data))
+//     .catch((error) => console.log(error))
+
+//! Async ve Await
+// async function getData() {
+//     const response = await fetch("https://jsonplaceholder.typicode.com/users")
+//     const data = await response.json()
+//     console.log(data) 
+// }
+
+const getData = async () => {
+    try {
+        const response = await fetch("https://jsonplaceholder.typicode.com/users")
+        const data = await response.json()
+        console.log(data)
+    } catch (error) {
+        console.log(error)
+    }
+} 
+
+getData()
